675b981dc56314ec7431d95d3d4a05b730b35176
WilsonWu <wilson.wu.yy@gmail.com>
2023-01-22 02:04:10 +0800

    init(service): 构建服务端框架

---
+++ packages/service/src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1674324099731_3132',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
