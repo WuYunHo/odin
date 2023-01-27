68d8df170bf9af1f59ba4fd38010d41451b2fd28
WilsonWu <wilson.wu.yy@gmail.com>
2023-01-27 16:43:09 +0800

    feat(service): 镜像打包

--- packages/service/src/config/config.default.ts
+++ packages/service/src/config/config.default.ts
@@ -21,7 +21,7 @@ export default {
         database:
           process.env.MIDWAY_SERVER_ENV === 'prod' ? 'db_release' : 'db_dev',
         synchronize: true, // 如果第一次使用，不存在表，有同步的需求可以写 true，注意会丢数据
-        logging: true,
+        logging: false,
 
         // 配置实体模型
         entities: [Product],
